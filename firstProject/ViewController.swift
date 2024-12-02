08f0da39a00d8d59578c2ecfed4d638e135638b4
Aviral Singh <149692321+the-number-17@users.noreply.github.com>
2024-12-02 12:14:55 +0530

    Initial Commit

---
+++ firstProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firstProject
+//
+//  Created by Batch - 1 on 02/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ firstProjectTests/firstProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  firstProjectTests.swift
+//  firstProjectTests
+//
+//  Created by Batch - 1 on 02/12/24.
+//
+
+import Testing
+@testable import firstProject
+
+struct firstProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
